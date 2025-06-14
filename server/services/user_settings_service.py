eb06142fa99bed9d30041dcbf6854e7abd5364ac
Dmitry <dmitryesin@hotmail.com>
2025-06-14 20:31:44 +0300

    Refactor import statements in user_settings_service.py

--- server/services/user_settings_service.py
+++ server/services/user_settings_service.py
@@ -1,8 +1,12 @@
-from db.user_settings_repository import save_user_settings_to_psql, get_user_settings_from_psql
+from db.user_settings_repository import (
+    save_user_settings_to_psql,
+    get_user_settings_from_psql,
+)
 
 
 async def save_user_settings(user_id: int, user_settings: dict):
     await save_user_settings_to_psql(user_id, user_settings)
+
 
 async def get_user_settings(user_id: int) -> dict:
     return await get_user_settings_from_psql(user_id)
