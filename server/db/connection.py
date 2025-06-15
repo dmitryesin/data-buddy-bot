819ffbf26953e7109f2c804091a29b91073297ab
Dmitry <dmitryesin@hotmail.com>
2025-06-15 19:33:38 +0300

    Fix syntax error in get_psql_connection function by adding a missing comma

--- server/db/connection.py
+++ server/db/connection.py
@@ -15,7 +15,7 @@ def get_psql_connection():
             user=os.getenv("DB_USERNAME"),
             password=os.getenv("DB_PASSWORD"),
             host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT")
+            port=os.getenv("DB_PORT"),
         )
     except psycopg2.OperationalError:
         logger.error("Failed to connect to database.")
