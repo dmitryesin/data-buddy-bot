8f34411329dcfde15bbde4be3c55e7d7c6db8136
Dmitry <dmitryesin@hotmail.com>
2025-06-15 17:48:48 +0300

    Add user_answers table to database schema

--- database/schema.sql
+++ database/schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE user_questions (
     question JSONB NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE user_answers (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    answer JSONB NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
